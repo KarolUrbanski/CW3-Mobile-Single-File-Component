360c102bf6ed17d4ef18cc68850e95fc61ab1e91
Karol Urbanski <karoll240@gmail.com>
2023-03-17 14:50:22 +0100

    configVit for deploy with githubpages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/CW3-Mobile-Single-File-Component/",
   plugins: [
     vue2(),
     legacy({
